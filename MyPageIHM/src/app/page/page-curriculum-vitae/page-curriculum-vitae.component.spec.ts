234fa930b4cd79fe9c0c2e1a5478d8b747438968
Theophile-Yvars <79571637+Theophile-Yvars@users.noreply.github.com>
2024-05-22 21:24:15 +0200

    init school component

--- MyPageIHM/src/app/page/page-curriculum-vitae/page-curriculum-vitae.component.spec.ts
+++ MyPageIHM/src/app/page/page-curriculum-vitae/page-curriculum-vitae.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PageCurriculumVitaeComponent } from './page-curriculum-vitae.component';
 
 describe('PageCurriculumVitaeComponent', () => {
--- MyPageIHM/src/app/page/page-curriculum-vitae/page-curriculum-vitae.component.ts
+++ MyPageIHM/src/app/page/page-curriculum-vitae/page-curriculum-vitae.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../components/header/header.component';
+import { SchoolComponent } from '../components/school/school.component';
+import { SchoolData } from '../../interfaces/school.interface';
 
 @Component({
   selector: 'app-page-curriculum-vitae',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    SchoolComponent,
+    CommonModule
   ],
   templateUrl: './page-curriculum-vitae.component.html',
   styleUrl: './page-curriculum-vitae.component.css'
 })
-export class PageCurriculumVitaeComponent {
 
+export class PageCurriculumVitaeComponent {
+  schools: SchoolData[] = [
+    { name: 'École 1', degree: 'Diplôme 1', startDate: '2010', endDate: '2012' },
+    { name: 'École 2', degree: 'Diplôme 2', startDate: '2012', endDate: '2014' },
+  ];
 }
