a9ebc1c87922a5f587f75af489777a23915cb788
Theophile-Yvars <79571637+Theophile-Yvars@users.noreply.github.com>
2024-05-23 21:00:15 +0200

    ajout des diplômes

--- MyPageIHM/src/app/page/page-curriculum-vitae/page-curriculum-vitae.component.ts
+++ MyPageIHM/src/app/page/page-curriculum-vitae/page-curriculum-vitae.component.ts
@@ -19,7 +19,13 @@ import { SchoolData } from '../../interfaces/school.interface';
 
 export class PageCurriculumVitaeComponent {
   schools: SchoolData[] = [
-    { name: 'École 1', degree: 'Diplôme 1', startDate: '2010', endDate: '2012' },
-    { name: 'École 2', degree: 'Diplôme 2', startDate: '2012', endDate: '2014' },
+    { name: 'Polytech Nice Sophia', degree: 'Diplôme d\'ingénieur en informatique : Mineur Architecture logiciel', startDate: '2021', endDate: '2024' },
+    { name: 'Université de Perpignan Via Domitia', degree: 'Diplôme licence informatique et mathématique', startDate: '2019', endDate: '2021' },
+    { name: 'IUT Nîmes et IUT 1 Grenoble', degree: 'Diplôme DUT Génie Electrique et Informatique Industrielle', startDate: '2017', endDate: '2019' },
+    { name: 'CFAI Formavenir Thyez', degree: 'Diplôme BAC Pro Maintenance des Equipements Industriels', startDate: '2016', endDate: '2017' },
+    { name: 'Lycée des métiers Porte des Alpes', degree: 'CAP Electricien', startDate: '2015', endDate: '2016' },
+    { name: 'CFA Compagnons du Tour de France', degree: 'CAP Charpente', startDate: '2013', endDate: '2014' },
+    { name: 'MFR Vulbens', degree: 'BAC Pro Métiers du commerce et de la vente', startDate: '2010', endDate: '2012' },
+    { name: 'MFR Vulbens', degree: 'CAP Equipier Polyvalent du Commerce', startDate: '2008', endDate: '2010' },
   ];
 }
